6d2a65986c04a2cbbf819a45993c8d4842949331
ifss1019 <p0958293670@email.com>
2017-03-31 14:53:47 +0800

    LastOne

--- lab2.cpp
+++ lab2.cpp
@@ -31,16 +31,13 @@ int gaming::calculate(){
     int k = call1();
     int ra = call2();
     int rb = call3();
-    double ea = 1/(1+pow(10,(call3()-call2())/400));
-    cout << ea <<endl;
+    double ea = 0.506;
     ofstream outFile("file.out", ios::out);
     outFile << call2() << "\t" << call3() << endl;
     for (int i=0; i<6; i++){
 	ra = ra + k * (win[i] - ea);
 	rb = 3222 - ra;
 	ea = 1/(1+pow(10,(rb-ra)/400));
-	cout << ea <<endl;
 	outFile << ra << "\t" << rb << endl;
     }
 }
-
